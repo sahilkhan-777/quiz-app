8a808c86c210379365208179ee3404380199fee5
Sahil Khan <sahil4t9@gmail.com>
2026-06-01 22:22:56 +0530

    Add score section and prev button, make prev and next buttons disable when there is no previous or next question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,6 +5,7 @@ function Quiz(){
     const [index, setIndex] = useState(0);
     const [answered, setAnswered] = useState(false);
     const [selected, setSelected] = useState(null);
+    const [score, setScore] = useState(0);
 
     function checkAnswer(event){
         event.preventDefault();
@@ -14,15 +15,25 @@ function Quiz(){
         }
         setAnswered(true);
         if(selected === quizData[index].answer){
-            alert("Correct!");
-        } else {
-            alert("Wrong! The correct answer is: " + quizData[index].answer);
+            setScore(prev => prev + 1);
         }
-        changeQuestion();
+        nextQuestion();
     }
 
-    function changeQuestion(){
+    function nextQuestion(){
         setIndex(prev => prev + 1);
+        setAnswered(false);
+        setSelected(null);
+    }
+
+    function prevQuestion(event){
+        setIndex(prev => prev - 1);
+        // if(index > 0){
+        //     setIndex(prev => prev - 1);
+        // }
+        // else{
+        //     event.target.disabled = true;
+        // }
         setAnswered(false);
         setSelected(null);
     }
@@ -30,7 +41,8 @@ function Quiz(){
     return(
         <div className="quiz">
             <h2>Quiz</h2>
-            <p>{quizData[index].question}</p>
+            <p>Score: {score}</p>
+            <p>{quizData[index].id}. {quizData[index].question}</p>
             <form onSubmit={checkAnswer}>
             {quizData[index].options.map((option, i) => (
                 <div key={i}>
@@ -46,7 +58,8 @@ function Quiz(){
                 </div>
             ))}
             <input type="submit" value="Submit" />
-            <button type="button" onClick={changeQuestion}>Next</button>
+            <button disabled={index === quizData.length -1} type="button" onClick={nextQuestion}>Next</button>
+            <button disabled={index === 0} type="button" onClick={prevQuestion}>Previous</button>
             </form>
         </div>
     );
