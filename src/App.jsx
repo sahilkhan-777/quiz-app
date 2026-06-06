e74b18694e513b0fe4bac220b48f2347d3756741
Sahil Khan <sahil4t9@gmail.com>
2026-06-06 23:20:24 +0530

    Move the score state up to the parent App component
    Pass two function props one to handle score on submitting answers and another one to reset score on clicking restart

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,25 @@ import Header from './components/Header';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
 import Footer from './components/Footer';
-import questions from "./questions"
+import questions from "./questions";
+import Score from './components/Score';
 
 function App() {
+  const [score, setScore] = useState(0);
+
+  function handleCorrectAnswer(){
+    setScore(prev => prev + 1);
+  }
+
+  function handleRestart(){
+    setScore(0);
+  }
 
   return (
     <>
       <Header />
-      <Quiz />
+      <Score currentScore={score}/>
+      <Quiz onCorrectAnswer={handleCorrectAnswer} onRestartQuiz={handleRestart} />
       <Result />
       <Footer />
     </>
