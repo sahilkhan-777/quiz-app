399f3ab24ab5713aeeab1d85b623b51a73fcf151
Sahil Khan <sahil4t9@gmail.com>
2026-06-06 23:13:54 +0530

    Add the new header icon into the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ function Header(){
     return(
         <>
         <header className="header">
-            <h1>Quiz App</h1>
+            <h1>
+                <span className="icon">
+                    <img src="src/assets/icon.svg" alt="icon"/>
+                </span>
+                Quiz App
+            </h1>
         </header>
         </>
     )
