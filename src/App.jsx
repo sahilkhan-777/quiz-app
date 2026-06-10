3ef00ca9d2ce07bfc264fda67e0d361b2274a9f2
Sahil Khan <sahil4t9@gmail.com>
2026-06-10 21:46:08 +0530

    Add a isCompleted state that controls which component to render, either the Quiz component or the QuizEnd component when its true
    Add a function to set the state of isCompleted and pass it to the Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
-import { useState } from 'react'
-import './App.css';
-import Header from './components/Header';
-import Quiz from './components/Quiz';
-import Result from './components/Result';
-import Footer from './components/Footer';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+import QuizEnd from "./components/QuizEnd";
+import Footer from "./components/Footer";
 import questions from "./questions";
-import Score from './components/Score';
+import Score from "./components/Score";
 
 function App() {
   const [score, setScore] = useState(0);
+  const [isCompleted, setIsCompleted] = useState(false);
 
-  function handleCorrectAnswer(){
-    setScore(prev => prev + 1);
+  function handleCorrectAnswer() {
+    setScore((prev) => prev + 1);
   }
 
-  function handleRestart(){
+  function handleCompleteQuiz() {
+    setIsCompleted(true);
+  }
+
+  function handleRestart() {
     setScore(0);
+    setIsCompleted(false);
   }
 
   return (
     <>
       <Header />
-      <Score currentScore={score}/>
-      <Quiz onCorrectAnswer={handleCorrectAnswer} onRestartQuiz={handleRestart} />
-      <Result />
+      <Score currentScore={score} />
+      {isCompleted && <QuizEnd score={score} onRestart={handleRestart} totalQuestions={questions.length} />}
+      {!isCompleted && (
+        <Quiz
+          onCorrectAnswer={handleCorrectAnswer}
+          onComplete={handleCompleteQuiz}
+        />
+      )}
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
