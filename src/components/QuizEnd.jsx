5d699dd3c740a8b0f6567c992c7caba536172a38
Sahil Khan <sahil4t9@gmail.com>
2026-06-10 21:51:10 +0530

    Add a message array and display it on the page based on the total score

--- src/components/QuizEnd.jsx
+++ src/components/QuizEnd.jsx
@@ -1,11 +1,29 @@
-function QuizEnd(props){
-    return(
-        <div className="quiz-end">
-            <h2>Quiz Completed!</h2>
-            <p>Your final score is: {props.finalScore} out of {props.totalQuestions}</p>
-            <button onClick={props.onRestart}>Restart Quiz</button>
-        </div>
-    )
+function QuizEnd(props) {
+  const messages = [
+    "Well done! You are a quiz master!",
+    "Great job! You have a good grasp of the material.",
+    "Not bad, keep practicing!",
+    "Better luck next time!",
+  ];
+
+  return (
+    <div className="quiz-end">
+      <h2>Quiz Completed!</h2>
+      <p>
+        Your answered {props.score} out of {props.totalQuestions} correctly.
+      </p>
+      <p>
+        {props.score === 10
+          ? messages[0]
+          : props.score >= 7
+            ? messages[1]
+            : props.score >= 5
+              ? messages[2]
+              : messages[3]}
+      </p>
+      <button onClick={props.onRestart}>Restart Quiz</button>
+    </div>
+  );
 }
 
 export default QuizEnd;
