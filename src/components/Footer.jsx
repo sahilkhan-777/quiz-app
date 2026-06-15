bf3b97ada5801a46d4e85e823635c9b7de505874
Sahil Khan <sahil4t9@gmail.com>
2026-06-15 21:41:05 +0530

    refractor: replace css classes with tailwind utility classes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <footer>
+        <footer className="bg-white text-blue-900 text-center">
             <p>&copy; 2023 Quiz App. All rights reserved.</p>
         </footer>
     )
