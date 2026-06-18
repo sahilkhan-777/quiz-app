eafd7ebf00c3eac43e927ae301d50835f50d2694
Sahil Khan <sahil4t9@gmail.com>
2026-06-18 23:28:16 +0530

    Add tailwind utility classes inside the elements

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -26,7 +26,7 @@ function Quiz(props) {
     setSelected(null);
   }
 
-  function prevQuestion(event) {
+  function prevQuestion() {
     setIndex((prev) => prev - 1);
     setAnswered(false);
     setSelected(null);
@@ -36,16 +36,9 @@ function Quiz(props) {
     props.onComplete();
   }
 
-  function handleRestart() {
-    setIndex(0);
-    props.onRestartQuiz();
-    setAnswered(false);
-    setSelected(null);
-  }
-
   return (
-    <div className="quiz">
-      <p className="question">{quizData[index].question}</p>
+    <div className="quiz shadow w-1/2 my-5 mx-auto bg-white rounded-xl py-4 px-5">
+      <p className="mb-3 text-2xl font-bold text-blue-950">{quizData[index].question}</p>
       <form onSubmit={checkAnswer}>
         {quizData[index].options.map((option, i) => (
           <div key={i}>
@@ -60,18 +53,18 @@ function Quiz(props) {
             />
             <label
               htmlFor={`option${i + 1}`}
-              className={`option-label ${answered && option === quizData[index].answer ? "correct" : ""} ${answered && option !== quizData[index].answer ? "incorrect" : ""}`}
+              className={`quiz-options ${answered && option === quizData[index].answer ? "correct" : ""} ${answered && option !== quizData[index].answer ? "incorrect" : ""}`}
             >
-              <span>{letters[i]}</span> {option}
+              <span className="text-xl text-blue-950 bg-yellow-400 py-1.5 px-3 rounded-full">{letters[i]}</span> {option}
             </label>
           </div>
         ))}
-        <div className="btn-group">
+        <div className="flex justify-between my-20 mx-0 gap-5">
           <button
             disabled={index === 0}
             type="button"
             onClick={prevQuestion}
-            className="prev-btn"
+            className="quiz-btns bg-neutral-300 hover:bg-neutral-400"
           >
             <svg
               viewBox="0 0 25 25"
@@ -94,11 +87,14 @@ function Quiz(props) {
             </svg>
             Previous
           </button>
-          <button type="submit" className="submit-btn">
+          <button type="submit" className="group submit-button quiz-btns bg-white hover:bg-green-600">
             <svg
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              stroke="currentColor"
+              stroke-width="1.6"
+              className="group-hover: text-white"
             >
               <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
               <g
@@ -116,14 +112,14 @@ function Quiz(props) {
             Submit
           </button>
           {index === quizData.length - 1 && answered ? 
-          <button type="button" onClick={completeQuiz} className="complete-btn">
+          <button type="button" onClick={completeQuiz} className="quiz-btns bg-yellow-500 hover:bg-yellow-600">
             Complete Quiz
           </button> : 
           <button
             disabled={!answered}
             type="button"
             onClick={nextQuestion}
-            className="next-btn"
+            className="quiz-btns bg-yellow-400 hover:bg-yellow-500"
           >
             Next
             <svg
