6167a0566bb1d5132f0dacb5de8906f3d6d31e6e
Sahil Khan <sahil4t9@gmail.com>
2026-06-24 21:47:13 +0530

    feat: remove svg bulb icon and replace it with bulb icon component from material icons library

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+
 function Header(){
     return(
         <header className="bg-yellow-400 text-blue-950 w-full">
-            <h1 className="py-5 flex justify-center uppercase text-6xl font-semibold">
-                <span className="mr-10 flex items-center">
-                    <img src="src/assets/icon.svg" alt="icon" className="w-16 h-16"/>
+            <h1 className="py-5 flex justify-center uppercase text-center text-6xl font-semibold">
+                <span className="mr-2 sm:mr-5 flex items-center">
+                    <LightbulbIcon sx={{ fontSize: 65 }}/>
                 </span>
                 Quiz App
             </h1>
